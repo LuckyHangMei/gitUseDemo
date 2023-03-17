b3d2e7bbfe6058bc9183e0fdd9cb1f3318b310de
hangshilong <hangshilong@richisland.com>
2023-03-17 15:56:36 +0800

    提交

---
+++ People.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAllTestDemo
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int ContionCount { get; set; }
+        public object fdg { get; set; }
+        public bool Marray { get; set; }
+    }
+    public class PeopleChild : People
+    {
+        public int Height { get; set; }
+        public int StudentNo { get; set; }
+    }
+
+    public class PeopleGraner
+    {
+        public People People { get; set; }
+        public string RoomName { get; set; }
+        public int RoomNo { get; set; }
+    }
+    public class FocusTagValue
+    {
+        public string Tag { get; set; }
+        public double[][] Value { get; set; }
+    }
+
+    public class VarList
+    {
+        public string Code { get; set; }
+        public List<string> Address { get; set; }
+    }
+}
